12f426adb6c67926bab1602281b47636ece5e532
amouhadi <e.amouhadi@gmail.com>
2018-07-11 00:12:14 -0700

    clean-up

--- ui/src/actions/index.js
+++ ui/src/actions/index.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
-const ROOT_URL = `https://badapi.iqvia.io/api/v1/Tweets`;
+const ROOT_URL = `http://localhost:3000`;
 
 export const INITIAL_FETCH_TWEETS = 'INITIAL_FETCH_TWEETS';
 export const SECONDARY_FETCH_TWEETS = 'SECONDARY_FETCH_TWEETS';
+export const SET_UNIQUE_TWEETS = 'SET_UNIQUE_TWEETS';
 
 export function fetchTweets(startDate, endDate) {
-    //console.log("end date in action "+endDate);
-    const url = `${ROOT_URL}?startDate=${startDate}&endDate=${endDate}`;
+    const url = `${ROOT_URL}/${startDate}/${endDate}`;
     const request = axios.get(url)
-    console.log(url);
-    console.log('Request:', request);
     return{
         type: INITIAL_FETCH_TWEETS,
         payload: request
@@ -17,13 +15,17 @@ export function fetchTweets(startDate, endDate) {
 }
 
 export function secondaryFetchTweets(startDate, endDate) {
-    //console.log("end date in action "+endDate);
-    const url = `${ROOT_URL}?startDate=${startDate}&endDate=${endDate}`;
+    const url = `${ROOT_URL}/${startDate}/${endDate}`;
     const request = axios.get(url)
-    console.log(url);
-    console.log('Request:', request);
     return{
         type: SECONDARY_FETCH_TWEETS,
         payload: request
+    };
+}
+
+export function setUniqueTweets(tweets) {
+    return{
+        type: SET_UNIQUE_TWEETS,
+        payload: tweets
     };
 }
--- ui/src/reducers/tweet_reducer.js
+++ ui/src/reducers/tweet_reducer.js
@@ -1,15 +1,33 @@
 import {
     INITIAL_FETCH_TWEETS,
     SECONDARY_FETCH_TWEETS,
+    SET_UNIQUE_TWEETS,
 } from '../actions/index';
-
-export default function(state = [], action) {
+const initialState = {
+    allTweets: [],
+    uniqueTweets: [],
+}
+export default function(state = initialState, action) {
     //console.log('Action recieved ', action);
     switch(action.type){
         case INITIAL_FETCH_TWEETS:
-            return [...action.payload.data];
+            return {
+                ...state,
+                allTweets: [...action.payload.data]
+            };
         case SECONDARY_FETCH_TWEETS:
-            return ([...state, ...action.payload.data])
+            return {
+                ...state,
+                allTweets:[
+                    ...state.allTweets,
+                    ...action.payload.data
+                ]
+            }
+        case SET_UNIQUE_TWEETS:
+            return{
+                ...state,
+                uniqueTweets: [...action.payload]
+            }
     }
     return state;
 }
